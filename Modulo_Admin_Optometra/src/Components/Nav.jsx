6faf349f4d924bd62085ed5b2038fb1809afa56e
Lorena Romero <lorenar1930@gmail.com>
2026-07-01 00:21:59 -0500

    Actualizando Nav.jsx

--- Modulo_Admin_Optometra/src/Components/Nav.jsx
+++ Modulo_Admin_Optometra/src/Components/Nav.jsx
@@ -55,48 +55,7 @@ function Nav() {
               </ul>
             </li>
 
-            {/* Dropdown Caja */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Caja
-              </a>
-              <ul className="dropdown-menu">
-                <li className="dropdown-header text-uppercase fs-7 fw-bold text-info">Clientes</li>
-                <li><Link className="dropdown-item" to="/caja/consultar-cliente">Consultar Cliente</Link></li>
-                <li><Link className="dropdown-item" to="/caja/RegistrarDatos">Registrar Datos</Link></li>
-
-                <li><Link className="dropdown-item" to="/caja/registrar-cliente">Registrar Cliente</Link></li>
-                <li><Link className="dropdown-item" to="/caja/agregar-consulta1">Agregar Consulta1</Link></li>
-                <li><Link className="dropdown-item" to="/caja/quitar-consulta">Quitar Consulta</Link></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li className="dropdown-header text-uppercase fs-7 fw-bold text-info">Productos</li>
-                <li><Link className="dropdown-item" to="/caja/agregar-productos">Agregar Productos</Link></li>
-                <li><Link className="dropdown-item" to="/caja/quitar-productos">Quitar Productos</Link></li>
-                <li><Link className="dropdown-item" to="/caja/producto-vendido">Productos Vendidos</Link></li>
-                <li><Link className="dropdown-item" to="/caja/cantidad-vendidos">Cantidad Vendidos</Link></li>
-                <li><Link className="dropdown-item" to="/caja/precio-producto">Precio del Producto</Link></li>
-                <li><Link className="dropdown-item" to="/caja/registrar-servicio">Registrar Servicio</Link></li>
-                <li><Link className="dropdown-item" to="/caja/registrar-pedido">Registrar Pedido</Link></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li className="dropdown-header text-uppercase fs-7 fw-bold text-info">Ventas</li>
-                <li><Link className="dropdown-item" to="/caja/confirmar-venta">Confirmar Venta</Link></li>
-                <li><Link className="dropdown-item" to="/caja/visualizar-venta">Visualizar Venta</Link></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li className="dropdown-header text-uppercase fs-7 fw-bold text-info">Formas de Pago</li>
-                <li><Link className="dropdown-item" to="/caja/efectivo">Efectivo</Link></li>
-                <li><Link className="dropdown-item" to="/caja/tarjeta-credito">Tarjeta de Crédito</Link></li>
-                <li><Link className="dropdown-item" to="/caja/tarjeta-debito">Tarjeta Débito</Link></li>
-                <li><Link className="dropdown-item" to="/caja/plataformas">Plataformas</Link></li>
-                <li><Link className="dropdown-item" to="/caja/confirmacion-banco">Confirmación de Banco</Link></li>
-              </ul>
-            </li>
-
+           
             {/* Dropdown Optómetra */}
             <li className="nav-item dropdown">
               <a
@@ -116,44 +75,7 @@ function Nav() {
               </ul>
             </li>
 
-            {/* Dropdown Compras */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Compras
-              </a>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/compras/proveedores">Proveedores</Link></li>
-                <li><Link className="dropdown-item" to="/compras/categorias">Categorías</Link></li>
-                <li><Link className="dropdown-item" to="/compras/registro">Registro de Compras</Link></li>
-              </ul>
-            </li>
-
-            {/* Dropdown Bodega */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Bodega
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/consultar-producto">
-                    Consultar producto
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
+           
 
           {/* 4. Botón de Cerrar Sesión con onClick */}
           <button
